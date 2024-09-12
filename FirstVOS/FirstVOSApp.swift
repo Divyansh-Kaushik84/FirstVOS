48ee6a5a9062fa654cde6b1db4a50e30b39240e1
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-09-13 03:36:20 +0530

    Initial Commit

---
+++ FirstVOS/FirstVOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVOSApp.swift
+//  FirstVOS
+//
+//  Created by Divyansh Kaushik on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstVOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
